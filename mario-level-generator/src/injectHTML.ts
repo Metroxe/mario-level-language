5cd482b4410a56087c7be9bf36d929b25d31d148
ksyeo1010 <ksyeo1996@gmail.com>
2019-09-10 16:51:37 -0700

    matrix

--- mario-level-generator/src/injectHTML.ts
+++ mario-level-generator/src/injectHTML.ts
@@ -1,7 +1,54 @@
 import {IElement} from "./index";
 
 function injectHTML(elements: IElement[]): string {
-	return `<h1>put all content here</h1>`;
+	let x = 5;
+	let y = 5;
+
+	let test = createMatrix(x, y);
+
+
+
+	let html = drawMatrix(test);
+
+	return html;
+}
+
+function drawMatrix(matrix) {
+	let htmlString = '';
+
+	for (let x = 0; x < matrix.length; x++) {
+		for (let y = 0; y < matrix.length; y++) {
+			switch (matrix[x][y]) {
+				case 0:
+				default:
+					htmlString += '<div class=".empty">&nbsp;</div>'
+			}
+		}
+	}
+
+	return htmlString;
+}
+
+/**
+ * creates matrix given x and y
+ * @param x
+ * @param y
+ */
+function createMatrix(x: number, y: number) {
+	const matrix = Array(x);
+	// all start with -1
+	matrix.fill(Array(y).fill(-1));
+
+	return matrix;
+}
+
+
+function searchMatrix(x: number, y: number) {
+
+}
+
+function replaceMatrix(x: number, y: number, matrix, add) {
+	matrix[x][y] = add;
 }
 
 export default injectHTML;
