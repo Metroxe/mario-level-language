5015722720820a5133e8b1a4a4c50e0aa74f8ef4
ksyeo1010 <ksyeo1996@gmail.com>
2019-09-17 14:58:27 -0700

    added pipes and clouds

--- mario-level-generator/src/injectHTML.ts
+++ mario-level-generator/src/injectHTML.ts
@@ -15,31 +15,55 @@ async function injectHTML(elements: IElement[], SpriteEnum: typeof Sprite, sprit
 	const SPRITE = {
 		GROUND: {
 			sx: 0 * DIMENSION_RATIO,
-			sy: 0 * DIMENSION_RATIO,
-			sw: DIMENSION_RATIO,
-			sh: DIMENSION_RATIO,
-			image: blockSprite
+			sy: 0 * DIMENSION_RATIO
 		},
 		QUESTION_MARK: {
 			sx: 24 * DIMENSION_RATIO,
-			sy: 0 * DIMENSION_RATIO,
-			sw: DIMENSION_RATIO,
-			sh: DIMENSION_RATIO,
-			image: blockSprite
+			sy: 0 * DIMENSION_RATIO
 		},
 		BRICK: {
 			sx: 1 * DIMENSION_RATIO,
-			sy: 0 * DIMENSION_RATIO,
-			sw: DIMENSION_RATIO,
-			sh: DIMENSION_RATIO,
-			image: blockSprite
+			sy: 0 * DIMENSION_RATIO
 		},
-		PIPE: {
+		PIPE_TR: {
+			sx: 1 * DIMENSION_RATIO,
+			sy: 8 * DIMENSION_RATIO
+		},
+		PIPE_TL: {
 			sx: 0 * DIMENSION_RATIO,
-			sy: 8 * DIMENSION_RATIO,
-			sw: 2 * DIMENSION_RATIO,
-			sh: 2 * DIMENSION_RATIO,
-			image: blockSprite
+			sy: 8 * DIMENSION_RATIO
+		},
+		PIPE_BR: {
+			sx: 1 * DIMENSION_RATIO,
+			sy: 9 * DIMENSION_RATIO
+		},
+		PIPE_BL: {
+			sx: 0 * DIMENSION_RATIO,
+			sy: 9 * DIMENSION_RATIO
+		},
+		CLOUD_TR: {
+			sx: 2 * DIMENSION_RATIO,
+			sy: 20 * DIMENSION_RATIO
+		},
+		CLOUD_TM: {
+			sx: 1 * DIMENSION_RATIO,
+			sy: 20 * DIMENSION_RATIO
+		},
+		CLOUD_TL: {
+			sx: 0 * DIMENSION_RATIO,
+			sy: 20 * DIMENSION_RATIO
+		},
+		CLOUD_BR: {
+			sx: 2 * DIMENSION_RATIO,
+			sy: 21 * DIMENSION_RATIO
+		},
+		CLOUD_BM: {
+			sx: 1 * DIMENSION_RATIO,
+			sy: 21 * DIMENSION_RATIO
+		},
+		CLOUD_BL: {
+			sx: 0 * DIMENSION_RATIO,
+			sy: 21 * DIMENSION_RATIO
 		}
 	};
 
@@ -74,6 +98,56 @@ async function injectHTML(elements: IElement[], SpriteEnum: typeof Sprite, sprit
 			x: 2,
 			y: 2,
 			sprite: SpriteEnum.QUESTION_MARK
+		},
+		{
+			x: 3,
+			y: 2,
+			sprite: SpriteEnum.PIPE_TL
+		},
+		{
+			x: 4,
+			y: 2,
+			sprite: SpriteEnum.PIPE_TR
+		},
+		{
+			x: 3,
+			y: 3,
+			sprite: SpriteEnum.PIPE_BL
+		},
+		{
+			x: 4,
+			y: 3,
+			sprite: SpriteEnum.PIPE_BR
+		},
+		{
+			x: 4,
+			y: 0,
+			sprite: SpriteEnum.CLOUD_TR
+		},
+		{
+			x: 3,
+			y: 0,
+			sprite: SpriteEnum.CLOUD_TM
+		},
+		{
+			x: 2,
+			y: 0,
+			sprite: SpriteEnum.CLOUD_TL
+		},
+		{
+			x: 4,
+			y: 1,
+			sprite: SpriteEnum.CLOUD_BR
+		},
+		{
+			x: 3,
+			y: 1,
+			sprite: SpriteEnum.CLOUD_BM
+		},
+		{
+			x: 2,
+			y: 1,
+			sprite: SpriteEnum.CLOUD_BL
 		}
 	];
 	elements = testElements;
@@ -102,10 +176,40 @@ async function injectHTML(elements: IElement[], SpriteEnum: typeof Sprite, sprit
 				case SpriteEnum.QUESTION_MARK:
 					sprite = SPRITE.QUESTION_MARK;
 					break;
+				case SpriteEnum.PIPE_TR:
+					sprite = SPRITE.PIPE_TR;
+					break;
+				case SpriteEnum.PIPE_TL:
+					sprite = SPRITE.PIPE_TL;
+					break;
+				case SpriteEnum.PIPE_BR:
+					sprite = SPRITE.PIPE_BR;
+					break;
+				case SpriteEnum.PIPE_BL:
+					sprite = SPRITE.PIPE_BL;
+					break;
+				case SpriteEnum.CLOUD_TR:
+					sprite = SPRITE.CLOUD_TR;
+					break;
+				case SpriteEnum.CLOUD_TM:
+					sprite = SPRITE.CLOUD_TM;
+					break;
+				case SpriteEnum.CLOUD_TL:
+					sprite = SPRITE.CLOUD_TL;
+					break;
+				case SpriteEnum.CLOUD_BR:
+					sprite = SPRITE.CLOUD_BR;
+					break;
+				case SpriteEnum.CLOUD_BM:
+					sprite = SPRITE.CLOUD_BM;
+					break;
+				case SpriteEnum.CLOUD_BL:
+					sprite = SPRITE.CLOUD_BL;
+					break;
 				default:
 					break;
 			}
-			context.drawImage(sprite.image, sprite.sx, sprite.sy, sprite.sw, sprite.sh, element.x * DIMENSION_RATIO, element.y * DIMENSION_RATIO, DIMENSION_RATIO, DIMENSION_RATIO);
+			context.drawImage(blockSprite, sprite.sx, sprite.sy, DIMENSION_RATIO, DIMENSION_RATIO, element.x * DIMENSION_RATIO, element.y * DIMENSION_RATIO, DIMENSION_RATIO, DIMENSION_RATIO);
 
 		}
 	}
