092f49b21c2427aca9227b0142f91f2e5a4167cc
Christopher Powroznik <chrispow96@gmail.com>
2019-09-17 22:00:38 -0700

    added test levels into it

--- mario-level-generator/src/index.ts
+++ mario-level-generator/src/index.ts
@@ -8,7 +8,7 @@ import spriteSheet from "./spritesheet";
 
 let css: string;
 
-async function generateImage(elements: IElement[]): Promise<string> {
+async function generateImage(elementsArray: Array<IElement[]>): Promise<string[]> {
 
 	// start puppeteer
 	const browser = await puppeteer.launch({
@@ -17,43 +17,47 @@ async function generateImage(elements: IElement[]): Promise<string> {
 		executablePath: process.env.CHROME_BIN || null,
 	});
 
-	const page = await browser.newPage();
+	const images: string[] = [];
+	for (const elements of elementsArray) {
+		const page = await browser.newPage();
 
-	// get css
-	if (!css) {
-		css = await readFileAsync(`${__dirname}/../stylesheet.css`, {encoding: 'utf8'})
+		// get css
+		if (!css) {
+			css = await readFileAsync(`${__dirname}/../stylesheet.css`, {encoding: 'utf8'})
+		}
+
+		// input HTML here
+		await page.setContent(`<head><style>${css}</style></head><body style='margin: 0'>
+		<div id='level'></div>
+		<script>
+			const elements = ${JSON.stringify(elements)};
+			const injectHTML = ${String(injectHTML)};
+			const se = ${JSON.stringify(Sprite)};
+			const spriteSheet = "${spriteSheet}";
+			injectHTML(elements, se, spriteSheet);
+		</script>
+		</body>`);
+
+		// take screenshot
+		const rect: any = await page.evaluate(selector => {
+			// injectHTML(elements, document);
+			const element = document.querySelector(selector);
+			const {x, y, width, height} = element.getBoundingClientRect();
+			return {left: x, top: y, width, height, id: element.id};
+		}, "#level");
+
+		const base64 = await page.screenshot({
+			encoding: 'base64',
+			type: "png",
+			clip: {x: rect.left, y: rect.top, width: rect.width, height: rect.height},
+			omitBackground: true,
+		});
+
+		await page.close();
+		images.push(`data:image/png;base64,${base64}`);
 	}
-
-	// input HTML here
-	await page.setContent(`<head><style>${css}</style></head><body style='margin: 0'>
-	<div id='level'></div>
-	<script>
-		const elements = ${JSON.stringify(elements)};
-		const injectHTML = ${String(injectHTML)};
-		const se = ${JSON.stringify(Sprite)};
-		const spriteSheet = "${spriteSheet}";
-		injectHTML(elements, se, spriteSheet);
-	</script>
-	</body>`);
-
-	// take screenshot
-	const rect: any = await page.evaluate(selector => {
-		// injectHTML(elements, document);
-		const element = document.querySelector(selector);
-		const {x, y, width, height} = element.getBoundingClientRect();
-		return {left: x, top: y, width, height, id: element.id};
-	}, "#level");
-
-	const base64 = await page.screenshot({
-		encoding: 'base64',
-		type: "png",
-		clip: {x: rect.left, y: rect.top, width: rect.width, height: rect.height},
-		omitBackground: true,
-	});
-
-	await page.close();
 	await browser.close();
-	return `data:image/png;base64,${base64}`
+	return images
 }
 
 export default generateImage;
