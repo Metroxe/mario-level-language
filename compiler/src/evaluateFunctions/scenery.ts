243f770bffcc818d623206bc47887c4c71f07038
Christopher Powroznik <chrispow96@gmail.com>
2019-09-21 11:39:32 -0700

    flag done

--- compiler/src/evaluateFunctions/scenery.ts
+++ compiler/src/evaluateFunctions/scenery.ts
@@ -1,5 +1,13 @@
-import { IElement } from "shared";
+import { IElement, SpriteCommand, Sprite } from "shared";
 
 export default (statement: string[]): IElement[] => {
-	return []
+	const elements: IElement[] = [];
+	const x = parseFloat(statement[1]);
+	const y = parseFloat(statement[3]);
+	let cloud: boolean = false;
+	if (statement[5] === SpriteCommand.CLOUD) {
+		cloud = true;
+	}
+	if (cloud)
+	return elements;
 }
