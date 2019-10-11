88e95304476974b435bf7019a6e85772a64d3ea8
ksyeo1010 <ksyeo1996@gmail.com>
2019-10-10 23:34:27 -0700

    fixed level errors

--- shared/src/mockLevels/level1_1.ts
+++ shared/src/mockLevels/level1_1.ts
@@ -1,7 +1,7 @@
 export default
 `
 #FIRST PART
-(0,14) CLOUD;
+(0,14) CLOUD 0;
 (0,0) -> (70, 1) GROUND;
 
 (16,6) QUESTION_MARK;
@@ -114,7 +114,7 @@ VAR B_FOUR [
 (157, 2) B_FOUR;
 (158, 2) B_THREE;
 (159, 2) B_TWO;
-(160, 2) BUSH;
+(160, 2) BUSH 0;
 (160, 2) B_ONE;
 
 (165, 2) PIPE 1;
