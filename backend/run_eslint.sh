cc235d8370b605f290c3c892c922b607aca1ab58
janezzzz <33816422+huanxinzzzz@users.noreply.github.com>
2019-11-13 11:56:14 -0800

    Create run_eslint.sh
    may still need to add chmod +x to solve the permission denied problem

---
+++ backend/run_eslint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+`npm i -g eslint`
+run_eslint() {
+    while [ "$1" ]; do
+        if [ -d "$1" ]; then
+            listit "$1"/*
+        else
+        	`eslint --no-eslintrc -c my-eslint.json -f json "$1"`
+        fi
+        shift
+    done
+}
+
+run_eslint $1
