9d5af4446d1b94d3bfc40860b3b793273957aba5
Christopher Powroznik <chrispow96@gmail.com>
2019-11-14 14:29:18 -0800

    partial commit of generation code

--- backend/src/services/compileCodeFromLint.ts
+++ backend/src/services/compileCodeFromLint.ts
@@ -1,7 +1,15 @@
 import { IFileLint } from "./linter";
 import { levels } from "shared";
+import gameVariables from "../game_variables";
 
 function compileCodeFromLint(file: IFileLint): string {
+	let code = "#" + file.fileName + "\n";
+	code += gameVariables + "\n";
+	code += "(0,0) START\n";
+	let lineCount = 0;
+	do {
+		code += `(${lineCount + 10}, 0)`
+	} while(lineCount < file.linesOfCode);
 	return levels.examples;
 }
 
